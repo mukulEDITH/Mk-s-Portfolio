302e73c71f41bdfa2757bdbb2631947c261c0ee9
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-02-22 19:26:39 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -41,7 +41,7 @@ const socialMediaLinks = {
   // facebook: "https://www.facebook.com/saad.pasta7",
   // medium: "https://medium.com/@saadpasta",
   // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
+  instagram:"https://www.instagram.com/mukulkaushalll/?hl=en",
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
 };
