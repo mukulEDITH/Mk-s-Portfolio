f36ed32490126bfe324d8a20e02beba83a624821
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-03-12 18:00:11 +0530

    Update Skills.js

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+ import React, {useContext} from "react";
 import "./Skills.scss";
 import SoftwareSkill from "../../components/softwareSkills/SoftwareSkill";
 import { skillsSection} from "../../portfolio";
@@ -32,6 +32,15 @@ export default function Skills() {
                 alt=" Working emoji"
                 src={require("../../assets/images/Image_20240109_113339_489.png")}
               ></img>
+              <div class="cube">
+  <div class="face face1"><img class='icon'src={require("../cubeasset/js.webp")} alt="Java Logo"/></div>
+  <div class="face face2"><img class='icon' src={require("../cubeasset/react jjjj.webp")} alt="React.js Logo"/></div>
+  <div class="face face3"><img class='icon' src={require("../cubeasset/node j.webp")} alt="Node.js Logo"/></div>
+  <div class="face face4"><img class='icon' src={require("../cubeasset/java.webp")} alt="JAvaLogo"/></div>
+  <div class="face face5"><img class='icon' src={require("../cubeasset/MongoDB_logo.webp")} alt="MongoDB Logo"/></div>
+  <div class="face face6"><img class='icon' src={require("../cubeasset/adobe-logo-1.webp")} alt="AEM Logo"/></div>
+</div>
+
 
               
             <p
